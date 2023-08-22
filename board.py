c5c6d72db4d61fcbfa5755156908cce06baccc5d
gmtommasini <gmtommasini@gmail.com>
2023-08-22 17:15:29 -0400

    working for 2 players

--- board.py
+++ board.py
@@ -1,13 +1,9 @@
+import copy
 from enum import Enum
 from color import yellow_b, gray_f
 
 
 INITIAL_POSITIONS = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-
-class Player(Enum):
-    x = 'X'
-    o = 'O'
 
 
 POSITIONS = {
@@ -25,15 +21,20 @@ POSITIONS = {
 
 class Board:
     def __init__(self):
-        self.positions = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        self.positions = copy.deepcopy(INITIAL_POSITIONS)
         self.available_positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.p1_starts: bool = True
         self.p1_plays: bool = True
-        self.not_finished = True
+        self.p1_moves: list = []
+        self.p2_moves: list = []
 
     def clean_board(self):
-        self.positions = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        self.positions = copy.deepcopy(INITIAL_POSITIONS)
         self.available_positions = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.p1_starts = not self.p1_starts  # switches starting player
+        self.p1_plays = self.p1_starts
+        self.p1_moves: list = []
+        self.p2_moves: list = []
 
     def print_position(self, pos):
         if pos in self.available_positions:
@@ -42,55 +43,85 @@ class Board:
             return pos
 
     def print_board(self):
-        pp = self.print_position
-        print(f"""
-  {pp(self.positions[0][0])} │ {pp(self.positions[0][1])} │ {pp(self.positions[0][2])}  
- ═══╪═══╪═══
-  {pp(self.positions[1][0])} │ {pp(self.positions[1][1])} │ {pp(self.positions[1][2])}  
- ═══╪═══╪═══
-  {pp(self.positions[2][0])} │ {pp(self.positions[2][1])} │ {pp(self.positions[2][2])}
-""")
+        print(self)
 
-    def make_a_play(self, player, position):
-        if position in self.available_positions:
-            p = POSITIONS[str(position)]
-            self.positions[p[0]][p[1]] = player
-            self.available_positions.remove(position)
-            self.p1_plays = not self.p1_plays
+    def __str__(self):
+        pp = self.print_position
+        return f"""
+            {pp(self.positions[0][0])} │ {pp(self.positions[0][1])} │ {pp(self.positions[0][2])}  
+           ═══╪═══╪═══
+            {pp(self.positions[1][0])} │ {pp(self.positions[1][1])} │ {pp(self.positions[1][2])}  
+           ═══╪═══╪═══
+            {pp(self.positions[2][0])} │ {pp(self.positions[2][1])} │ {pp(self.positions[2][2])}
+        """
+
+    def available_moves(self) -> list:
+        return self.available_positions
+
+    def make_play(self, player: str, position: int ):
+        """
+        Make a play for x or o. Return False in case of match not finished, or
+        Return 0 for draw, or
+        Returns the number of the winning player
+        :param player: 'x' or 'o'
+        :param position: 1-9
+        :return: False, 0, 1, or 2
+        """
+
+        # Invalid play
+        if position not in self.available_positions:
+            return False
+
+        p = POSITIONS[str(position)]
+        self.positions[p[0]][p[1]] = player
+        self.available_positions.remove(position)
+
         if self.check_win():
-            self.not_finished = False
+            # returning the winner of the round
+            return 1 if self.p1_plays else 2
+
+        # No more moves = draw
+        if len(self.available_positions) == 0:
+            print(len(self.available_positions))
+            return 0
+
+        # Save moves
+        if player == 'x':
+            self.p1_moves.append(position)
+        elif player == 'o':
+            self.p2_moves.append(position)
+
+        # Alternating player
+        self.p1_plays = not self.p1_plays  # alternate turns
+        return False
 
     def check_win(self):
         p = self.positions
         for i in [0,1,2]:
             # checking lines:
             if p[i][0] == p[i][1] == p[i][2]:
-                print(f"WIN on line {i+1}")
                 p[i][0] = p[i][1] = p[i][2] = yellow_b(p[i][0])
                 return True
             # checking columns:
             if p[0][i] == p[1][i] == p[2][i]:
-                print(f"WIN on column {i+1}")
                 p[0][i] = p[1][i] = p[2][i] = yellow_b(p[2][i])
                 return True
 
         # checking diagonals
         if p[0][0] == p[1][1] == p[2][2]:
             p[0][0] = p[1][1] = p[2][2] = yellow_b(p[2][2])
-            print("WIN on primary diagonal")
             return True
         if p[0][2] == p[1][1] == p[2][0]:
             p[0][2] = p[1][1] = p[2][0] = yellow_b(p[2][0])
-            print("WIN on secondary  diagonal")
             return True
 
 
 if __name__ == '__main__':
     b = Board()
-    b.make_a_play('x', 1)
-    b.make_a_play('o', 2)
-    b.make_a_play('x', 9)
-    b.make_a_play('o', 4)
-    b.make_a_play('x', 5)
+    b.make_play('x', 1)
+    b.make_play('o', 2)
+    b.make_play('x', 9)
+    b.make_play('o', 4)
+    b.make_play('x', 5)
     # b.make_a_play('o', 7)
     b.print_board()
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import os
 from board import Board
+from scoreboard import ScoreBoard
 
 
 b = Board()
+score = ScoreBoard()
+
+
+def print_screen():
+    clear()
+    print(score)
+    b.print_board()
 
 
 def clear():
@@ -13,7 +21,7 @@ def get_user_input(prompt):
     while True:
         try:
             value = int(input(prompt))
-            if 1 <= value <= 9:
+            if 0 <= value <= 9:
                 return value
             else:
                 print("Invalid choice. Please chose an available position.")
@@ -24,11 +32,27 @@ def get_user_input(prompt):
 if __name__ == '__main__':
     keep_playing = True
     while keep_playing:
-        while b.not_finished:
-            clear()
-            b.print_board()
+        while True:
+            print_screen()
             (symbol, player) = ('x', 'Player 1') if b.p1_plays else ('o', 'Player 2')
-            user_input = get_user_input(f"{player}: choose the board position you want to play (a number between 1 and 9): ")
-            b.make_a_play(symbol, user_input)
+            user_input = get_user_input(f"{player}: choose the board position you want to play\n(a number between 1 and 9. 0 to end round): ")
+            if user_input == 0:
+                break
+            if winner := b.make_play(symbol, user_input):
+                score.score(winner)
+                print_screen()
+                if winner == 0:
+                    print("DRAW!")
+                else:
+                    print(f"Player {'1' if winner == 1 else '2'} wins the round.")
+                break
+        if keep_playing := True if input("Keep playing? (y/n)") == 'y' else False:
+            b.clean_board()
+        else:
+            # ENDING game
+            if winner := score.winning():
+                print("The winner is", winner)
+            else:
+                print("It is a Draw!!")
 
-        keep_playing = True if input("Keep playing? (y/n)") == 'y' else False
+
